9b05dea0b095b797609af66b2262a6ad5a838ba2
MarianaSequeira <mfs98@live.com.pt>
2021-06-15 10:29:25 +0100

    Setting up the project by adding Springboot framework

---
+++ src/main/java/codacyChallenge/CodacyChallengeApplication.java
@@ -0,0 +1,13 @@
+package codacyChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodacyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodacyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codacyChallenge/CodacyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package codacyChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodacyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
